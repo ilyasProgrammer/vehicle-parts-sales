c26aa67f50b21d3f8f298a0a523d5a82695437d6
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-06-20 14:30:44 +0300

    post

--- fleet_parts/controllers/main.py
+++ fleet_parts/controllers/main.py
@@ -12,3 +12,5 @@ class FleetPartsAPI(http.Controller):
     @http.route('/web/load_part', type='json',  auth="none", csrf=False)
     def load_part(self, uid, db, vehicle_id, vin, hash, data):
         print 'OK VERY GOOD'
+        return {'ok':'ok'}
+
