2c6c7d2d2fca728f66d59032cce00be3e9113b41
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-06-22 14:01:05 +0300

    imp sync

--- fleet_parts/controllers/main.py
+++ fleet_parts/controllers/main.py
@@ -34,3 +34,53 @@ class FleetPartsAPI(http.Controller):
                 _logger.info("Old product found and updated: %s", r['name'])
 
         return '{"response": "OK"}'
+
+    @http.route('/web/load_part_image', type='http',  auth="public", csrf=False, website=True)
+    def load_part_image(self, uid, db, vin, hash, data):
+        # Add product.product
+        product = self.env['product.product']
+        if not data:
+            return
+        for r in data['result']:
+            found = product.search([('c_id', '=', r['id'])])
+            if not found:
+                product.create(r)
+                _logger.info("New product created: %s", r['name'])
+            else:
+                found[0].write(r)
+                _logger.info("Old product found and updated: %s", r['name'])
+        for r in data['result']['part']:
+            found = product.search([('c_id', '=', r['id'])])
+            if not found:
+                product.create(r)
+                _logger.info("New product created: %s", r['name'])
+            else:
+                found[0].write(r)
+                _logger.info("Old product found and updated: %s", r['name'])
+
+        return '{"response": "OK"}'
+
+    @http.route('/web/delete_part', type='http',  auth="public", csrf=False, website=True)
+    def delete_part(self, uid, db, vin, hash, data):
+        # Add product.product
+        product = self.env['product.product']
+        if not data:
+            return
+        for r in data['result']:
+            found = product.search([('c_id', '=', r['id'])])
+            if not found:
+                product.create(r)
+                _logger.info("New product created: %s", r['name'])
+            else:
+                found[0].write(r)
+                _logger.info("Old product found and updated: %s", r['name'])
+        for r in data['result']['part']:
+            found = product.search([('c_id', '=', r['id'])])
+            if not found:
+                product.create(r)
+                _logger.info("New product created: %s", r['name'])
+            else:
+                found[0].write(r)
+                _logger.info("Old product found and updated: %s", r['name'])
+
+        return '{"response": "OK"}'
